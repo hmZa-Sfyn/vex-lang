08f68658ed848866293bc5a645bf45e278ef269f
hmZa-Sfyn <hamza.enchant@gmail.com>
2026-03-09 22:07:18 +0500

    v0.0.4

--- methods.go
+++ methods.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"strings"
 )
@@ -27,53 +28,79 @@ func stringMethod(v *Value, key string) *Value {
 	case "split":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
 			sep := ""
-			if len(args) > 0 { sep = args[0].Str }
+			if len(args) > 0 {
+				sep = args[0].Str
+			}
 			parts := strings.Split(s, sep)
 			arr := make([]*Value, len(parts))
-			for i, p := range parts { arr[i] = StringVal(p) }
+			for i, p := range parts {
+				arr[i] = StringVal(p)
+			}
 			return ArrayVal(arr), nil
 		}}
 	case "contains":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
-			if len(args) == 0 { return BoolVal(false), nil }
+			if len(args) == 0 {
+				return BoolVal(false), nil
+			}
 			return BoolVal(strings.Contains(s, args[0].Str)), nil
 		}}
 	case "starts_with":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
-			if len(args) == 0 { return BoolVal(false), nil }
+			if len(args) == 0 {
+				return BoolVal(false), nil
+			}
 			return BoolVal(strings.HasPrefix(s, args[0].Str)), nil
 		}}
 	case "ends_with":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
-			if len(args) == 0 { return BoolVal(false), nil }
+			if len(args) == 0 {
+				return BoolVal(false), nil
+			}
 			return BoolVal(strings.HasSuffix(s, args[0].Str)), nil
 		}}
 	case "replace":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
-			if len(args) < 2 { return StringVal(s), nil }
+			if len(args) < 2 {
+				return StringVal(s), nil
+			}
 			return StringVal(strings.ReplaceAll(s, args[0].Str, args[1].Str)), nil
 		}}
 	case "index":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
-			if len(args) == 0 { return NumberVal(-1), nil }
+			if len(args) == 0 {
+				return NumberVal(-1), nil
+			}
 			return NumberVal(float64(strings.Index(s, args[0].Str))), nil
 		}}
 	case "slice":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
 			runes := []rune(s)
 			start, end := 0, len(runes)
-			if len(args) > 0 { start = int(args[0].Num) }
-			if len(args) > 1 { end = int(args[1].Num) }
-			if start < 0 { start = 0 }
-			if end > len(runes) { end = len(runes) }
-			if start > end { start = end }
+			if len(args) > 0 {
+				start = int(args[0].Num)
+			}
+			if len(args) > 1 {
+				end = int(args[1].Num)
+			}
+			if start < 0 {
+				start = 0
+			}
+			if end > len(runes) {
+				end = len(runes)
+			}
+			if start > end {
+				start = end
+			}
 			return StringVal(string(runes[start:end])), nil
 		}}
 	case "bytes":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
 			bs := []byte(s)
 			arr := make([]*Value, len(bs))
-			for i, b := range bs { arr[i] = NumberVal(float64(b)) }
+			for i, b := range bs {
+				arr[i] = NumberVal(float64(b))
+			}
 			return ArrayVal(arr), nil
 		}}
 	}
@@ -92,14 +119,18 @@ func arrayMethod(v *Value, key string) *Value {
 		}}
 	case "pop":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
-			if len(v.Array) == 0 { return NullVal(), nil }
+			if len(v.Array) == 0 {
+				return NullVal(), nil
+			}
 			last := v.Array[len(v.Array)-1]
 			v.Array = v.Array[:len(v.Array)-1]
 			return last, nil
 		}}
 	case "shift":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
-			if len(v.Array) == 0 { return NullVal(), nil }
+			if len(v.Array) == 0 {
+				return NullVal(), nil
+			}
 			first := v.Array[0]
 			v.Array = v.Array[1:]
 			return first, nil
@@ -107,95 +138,143 @@ func arrayMethod(v *Value, key string) *Value {
 	case "join":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
 			sep := ""
-			if len(args) > 0 { sep = args[0].Str }
+			if len(args) > 0 {
+				sep = args[0].Str
+			}
 			parts := make([]string, len(arr))
-			for i, el := range arr { parts[i] = el.String() }
+			for i, el := range arr {
+				parts[i] = el.String()
+			}
 			return StringVal(strings.Join(parts, sep)), nil
 		}}
 	case "map":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
-			if len(args) == 0 { return v, nil }
+			if len(args) == 0 {
+				return v, nil
+			}
 			fn := args[0]
 			result := make([]*Value, len(arr))
 			for i, el := range arr {
 				r, err := callValue(fn, []*Value{el, NumberVal(float64(i))}, ci)
-				if err != nil { return nil, err }
+				if err != nil {
+					return nil, err
+				}
 				result[i] = r
 			}
 			return ArrayVal(result), nil
 		}}
 	case "filter":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
-			if len(args) == 0 { return v, nil }
+			if len(args) == 0 {
+				return v, nil
+			}
 			fn := args[0]
 			var result []*Value
 			for i, el := range arr {
 				r, err := callValue(fn, []*Value{el, NumberVal(float64(i))}, ci)
-				if err != nil { return nil, err }
-				if r.IsTruthy() { result = append(result, el) }
+				if err != nil {
+					return nil, err
+				}
+				if r.IsTruthy() {
+					result = append(result, el)
+				}
 			}
 			return ArrayVal(result), nil
 		}}
 	case "reduce":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
-			if len(args) < 2 { return NullVal(), nil }
+			if len(args) < 2 {
+				return NullVal(), nil
+			}
 			fn := args[0]
 			acc := args[1]
 			for i, el := range arr {
 				r, err := callValue(fn, []*Value{acc, el, NumberVal(float64(i))}, ci)
-				if err != nil { return nil, err }
+				if err != nil {
+					return nil, err
+				}
 				acc = r
 			}
 			return acc, nil
 		}}
 	case "find":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
-			if len(args) == 0 { return NullVal(), nil }
+			if len(args) == 0 {
+				return NullVal(), nil
+			}
 			fn := args[0]
 			for i, el := range arr {
 				r, err := callValue(fn, []*Value{el, NumberVal(float64(i))}, ci)
-				if err != nil { return nil, err }
-				if r.IsTruthy() { return el, nil }
+				if err != nil {
+					return nil, err
+				}
+				if r.IsTruthy() {
+					return el, nil
+				}
 			}
 			return NullVal(), nil
 		}}
 	case "includes":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
-			if len(args) == 0 { return BoolVal(false), nil }
+			if len(args) == 0 {
+				return BoolVal(false), nil
+			}
 			for _, el := range arr {
-				if el.Equals(args[0]) { return BoolVal(true), nil }
+				if el.Equals(args[0]) {
+					return BoolVal(true), nil
+				}
 			}
 			return BoolVal(false), nil
 		}}
 	case "slice":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
 			start, end := 0, len(arr)
-			if len(args) > 0 { start = int(args[0].Num) }
-			if len(args) > 1 { end = int(args[1].Num) }
-			if start < 0 { start = 0 }
-			if end > len(arr) { end = len(arr) }
-			if start > end { start = end }
+			if len(args) > 0 {
+				start = int(args[0].Num)
+			}
+			if len(args) > 1 {
+				end = int(args[1].Num)
+			}
+			if start < 0 {
+				start = 0
+			}
+			if end > len(arr) {
+				end = len(arr)
+			}
+			if start > end {
+				start = end
+			}
 			return ArrayVal(arr[start:end]), nil
 		}}
 	case "flat":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
 			var flat []*Value
 			for _, el := range arr {
-				if el.Type == VAL_ARRAY { flat = append(flat, el.Array...) } else { flat = append(flat, el) }
+				if el.Type == VAL_ARRAY {
+					flat = append(flat, el.Array...)
+				} else {
+					flat = append(flat, el)
+				}
 			}
 			return ArrayVal(flat), nil
 		}}
 	case "reverse":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
 			cp := make([]*Value, len(arr))
-			for i, el := range arr { cp[len(arr)-1-i] = el }
+			for i, el := range arr {
+				cp[len(arr)-1-i] = el
+			}
 			return ArrayVal(cp), nil
 		}}
 	case "first":
-		if len(arr) == 0 { return NullVal() }
+		if len(arr) == 0 {
+			return NullVal()
+		}
 		return arr[0]
 	case "last":
-		if len(arr) == 0 { return NullVal() }
+		if len(arr) == 0 {
+			return NullVal()
+		}
 		return arr[len(arr)-1]
 	}
 	return NullVal()
@@ -203,7 +282,9 @@ func arrayMethod(v *Value, key string) *Value {
 
 func responseMethod(v *Value, key string) *Value {
 	resp := v.Resp
-	if resp == nil { return NullVal() }
+	if resp == nil {
+		return NullVal()
+	}
 	switch key {
 	case "status":
 		return NumberVal(float64(resp.StatusCode))
@@ -222,25 +303,35 @@ func responseMethod(v *Value, key string) *Value {
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
 			defer resp.Body.Close()
 			b, err := io.ReadAll(resp.Body)
-			if err != nil { return ErrVal(err.Error()), nil }
+			if err != nil {
+				return ErrVal(err.Error()), nil
+			}
 			return StringVal(string(b)), nil
 		}}
 	case "json":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
 			defer resp.Body.Close()
 			b, err := io.ReadAll(resp.Body)
-			if err != nil { return ErrVal(err.Error()), nil }
+			if err != nil {
+				return ErrVal(err.Error()), nil
+			}
 			val, err := parseJSON(string(b))
-			if err != nil { return ErrVal(fmt.Sprintf("json parse error: %s", err.Error())), nil }
+			if err != nil {
+				return ErrVal(fmt.Sprintf("json parse error: %s", err.Error())), nil
+			}
 			return val, nil
 		}}
 	case "bytes":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
 			defer resp.Body.Close()
 			b, err := io.ReadAll(resp.Body)
-			if err != nil { return ErrVal(err.Error()), nil }
+			if err != nil {
+				return ErrVal(err.Error()), nil
+			}
 			arr := make([]*Value, len(b))
-			for i, by := range b { arr[i] = NumberVal(float64(by)) }
+			for i, by := range b {
+				arr[i] = NumberVal(float64(by))
+			}
 			return ArrayVal(arr), nil
 		}}
 	}
@@ -249,7 +340,9 @@ func responseMethod(v *Value, key string) *Value {
 
 func requestMethod(v *Value, key string) *Value {
 	req := v.Req
-	if req == nil { return NullVal() }
+	if req == nil {
+		return NullVal()
+	}
 	switch key {
 	case "method":
 		return StringVal(req.Method)
@@ -278,31 +371,90 @@ func requestMethod(v *Value, key string) *Value {
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
 			defer req.Body.Close()
 			b, err := io.ReadAll(req.Body)
-			if err != nil { return ErrVal(err.Error()), nil }
+			if err != nil {
+				return ErrVal(err.Error()), nil
+			}
 			return StringVal(string(b)), nil
 		}}
 	case "json":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
 			defer req.Body.Close()
 			b, err := io.ReadAll(req.Body)
-			if err != nil { return ErrVal(err.Error()), nil }
+			if err != nil {
+				return ErrVal(err.Error()), nil
+			}
 			val, err := parseJSON(string(b))
-			if err != nil { return ErrVal(fmt.Sprintf("json parse error: %s", err.Error())), nil }
+			if err != nil {
+				return ErrVal(fmt.Sprintf("json parse error: %s", err.Error())), nil
+			}
 			return val, nil
 		}}
 	case "param":
 		return &Value{Type: VAL_BUILTIN, Builtin: func(args []*Value, ci *CallInfo) (*Value, error) {
-			if len(args) == 0 { return NullVal(), nil }
+			if len(args) == 0 {
+				return NullVal(), nil
+			}
 			return StringVal(req.URL.Query().Get(args[0].Str)), nil
 		}}
 	case "remote_addr":
 		if req.RemoteAddr != "" {
-			h, _, _ := net.SplitHostPort(req.RemoteAddr)
+			h, _, _ := SplitHostPort(req.RemoteAddr)
 			return StringVal(h)
 		}
 		return NullVal()
 	}
 	return NullVal()
+}
+
+// SplitHostPort safely splits "host:port" string into host and port
+// Returns empty strings and error if format is invalid
+func SplitHostPort(addr string) (host, port string, err error) {
+	if addr == "" {
+		return "", "", nil
+	}
+
+	host, port, err = net.SplitHostPort(addr)
+	if err == nil {
+		return host, port, nil
+	}
+
+	// Common fallback cases when net.SplitHostPort fails
+	switch {
+	case addr[0] == ':':
+		// ":8080" style → host is empty
+		return "", addr[1:], nil
+
+	case addr[0] == '[':
+		// IPv6 with port: "[2001:db8::1]:8080"
+		// or just "[2001:db8::1]" without port
+		endBracket := len(addr) - 1
+		if addr[endBracket] == ']' {
+			// No port
+			return addr[1:endBracket], "", nil
+		}
+		// Has port after ]
+		if addr[endBracket-1] == ']' && addr[endBracket] == ':' {
+			// Actually malformed, but let's be nice
+			return addr[1 : endBracket-1], "", nil
+		}
+	}
+
+	// Most common case: no port at all (just IP or hostname)
+	return addr, "", nil
+}
+
+// ────────────────────────────────────────────────
+//           Convenience wrappers
+// ────────────────────────────────────────────────
+
+func HostOnly(addr string) string {
+	h, _, _ := SplitHostPort(addr)
+	return h
+}
+
+func PortOnly(addr string) string {
+	_, p, _ := SplitHostPort(addr)
+	return p
 }
 
 // callValue calls a vex value as a function
@@ -324,18 +476,28 @@ func callValue(fn *Value, args []*Value, ci *CallInfo) (*Value, error) {
 				env.Def(param.Name, args[i], false)
 			} else if param.Default != nil {
 				res := interp.evalExpr(param.Default, env)
-				if res.Err != nil { return nil, res.Err }
+				if res.Err != nil {
+					return nil, res.Err
+				}
 				env.Def(param.Name, res.Value, false)
 			} else {
 				env.Def(param.Name, NullVal(), false)
 			}
 		}
 		var body *BlockStmt
-		if fn.Fn != nil { body = fn.Fn.Body }
-		if fn.FnDecl != nil { body = fn.FnDecl.Body }
+		if fn.Fn != nil {
+			body = fn.Fn.Body
+		}
+		if fn.FnDecl != nil {
+			body = fn.FnDecl.Body
+		}
 		res := interp.execBlock(body, env)
-		if res.Err != nil { return nil, res.Err }
-		if res.Value != nil { return res.Value, nil }
+		if res.Err != nil {
+			return nil, res.Err
+		}
+		if res.Value != nil {
+			return res.Value, nil
+		}
 		return NullVal(), nil
 	}
 	return NullVal(), fmt.Errorf("value of type %v is not callable", fn.Type)
